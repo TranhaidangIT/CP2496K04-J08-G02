c70337615641e4813a583eb3ae24576957603fdd
BUNNYOI <ndnhana24103@cusc.ctu.edu.vn>
2025-08-10 07:06:18 +0700

    fullcodefinal

---
+++ cinema_management.sql
@@ -0,0 +1,242 @@
+CREATE DATABASE cinema_management;
+GO
+
+USE cinema_management;
+GO
+
+-- USERS
+CREATE TABLE users (
+    userId INT IDENTITY(1,1) PRIMARY KEY,
+    employeeId VARCHAR(10) UNIQUE NOT NULL,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    passwordHash VARCHAR(255) NOT NULL,
+    fullName NVARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    role VARCHAR(20) CHECK (role IN ('Admin', 'Manager', 'Employee')) NOT NULL,
+    createdAt DATETIME DEFAULT GETDATE()
+);
+
+-- PASSWORD RESET
+CREATE TABLE passwordResetRequests (
+    requestId INT IDENTITY(1,1) PRIMARY KEY,
+    staffId INT NOT NULL,
+    requestTime DATETIME DEFAULT GETDATE(),
+    status VARCHAR(20) DEFAULT 'Pending' CHECK (status IN ('Pending', 'Approved', 'Rejected')),
+    handledBy INT,
+    handledTime DATETIME,
+    FOREIGN KEY (staffId) REFERENCES users(userId),
+    FOREIGN KEY (handledBy) REFERENCES users(userId)
+);
+
+-- MOVIES
+CREATE TABLE movies (
+    movieId INT IDENTITY(1,1) PRIMARY KEY,
+    title NVARCHAR(255) NOT NULL,
+    duration INT NOT NULL CHECK (duration > 0),
+    genre VARCHAR(100),
+    description TEXT,
+    directedBy VARCHAR(100),
+    language VARCHAR(50),
+    poster VARCHAR(255),
+    ageRating VARCHAR(10),
+    releaseDate DATE NOT NULL,
+    createdAt DATETIME DEFAULT GETDATE()
+);
+
+-- ROOM TYPES
+CREATE TABLE roomTypes (
+    roomTypeId INT IDENTITY(1,1) PRIMARY KEY,
+    typeName NVARCHAR(50) UNIQUE NOT NULL,
+    description NVARCHAR(255),
+    maxRows INT CHECK (maxRows > 0),
+    maxColumns INT CHECK (maxColumns > 0)
+);
+
+-- SCREENING ROOMS
+CREATE TABLE screeningRooms (
+    roomId INT IDENTITY(1,1) PRIMARY KEY,
+    roomNumber VARCHAR(20) UNIQUE NOT NULL,
+    seatingLayout TEXT NOT NULL,
+    totalCapacity INT NOT NULL CHECK (totalCapacity > 0),
+    roomTypeId INT NOT NULL,
+    equipment TEXT,
+    roomStatus VARCHAR(20) NOT NULL DEFAULT 'Available'
+        CHECK (roomStatus IN ('Available', 'Maintenance', 'Unavailable')),
+    createdAt DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (roomTypeId) REFERENCES roomTypes(roomTypeId)
+);
+
+-- SHOWTIMES
+CREATE TABLE showtimes (
+    showtimeId INT IDENTITY(1,1) PRIMARY KEY,
+    movieId INT NOT NULL,
+    roomId INT NOT NULL,
+    showDate DATE NOT NULL,
+    showTime TIME NOT NULL,
+    endTime TIME,
+    createdAt DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (movieId) REFERENCES movies(movieId),
+    FOREIGN KEY (roomId) REFERENCES screeningRooms(roomId)
+);
+
+-- SERVICE CATEGORIES
+CREATE TABLE serviceCategories (
+    categoryId INT IDENTITY(1,1) PRIMARY KEY,
+    categoryName NVARCHAR(50) UNIQUE NOT NULL
+);
+
+-- SERVICES
+CREATE TABLE services (
+    serviceId INT IDENTITY(1,1) PRIMARY KEY,
+    serviceName NVARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
+    categoryId INT NOT NULL,
+    createdAt DATETIME DEFAULT GETDATE(),
+	img VARCHAR(255),
+    FOREIGN KEY (categoryId) REFERENCES serviceCategories(categoryId)
+);
+
+-- LOCKERS
+CREATE TABLE lockers (
+    lockerId INT IDENTITY(1,1) PRIMARY KEY,
+    lockerNumber VARCHAR(20) UNIQUE NOT NULL,
+    locationInfo NVARCHAR(255),
+    status VARCHAR(20) NOT NULL DEFAULT 'Available'
+        CHECK (status IN ('Available', 'Occupied', 'Maintenance')),
+    createdAt DATETIME DEFAULT GETDATE()
+);
+
+-- TICKETS
+CREATE TABLE tickets (
+    ticketId INT IDENTITY(1,1) PRIMARY KEY,
+    ticketCode VARCHAR(20) UNIQUE NOT NULL,
+    customerName NVARCHAR(100),
+    ticketType VARCHAR(50),
+    showtimeId INT NOT NULL,
+    soldBy INT NOT NULL,
+    soldAt DATETIME DEFAULT GETDATE(),
+    totalPrice DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (showtimeId) REFERENCES showtimes(showtimeId),
+    FOREIGN KEY (soldBy) REFERENCES users(userId)
+);
+
+-- LOCKER ASSIGNMENTS
+CREATE TABLE lockerAssignments (
+    assignmentId INT IDENTITY(1,1) PRIMARY KEY,
+    lockerId INT NOT NULL,
+    pinCode CHAR(4) NOT NULL,
+    itemDescription NVARCHAR(255),
+    assignedAt DATETIME DEFAULT GETDATE(),
+    releasedAt DATETIME NULL,
+    ticketCode VARCHAR(20),
+    FOREIGN KEY (lockerId) REFERENCES lockers(lockerId),
+    FOREIGN KEY (ticketCode) REFERENCES tickets(ticketCode)
+);
+
+-- LOCKER HISTORY
+CREATE TABLE lockerHistory (
+    historyId INT IDENTITY(1,1) PRIMARY KEY,
+    lockerId INT NOT NULL,
+    userAction NVARCHAR(100),
+    actionTime DATETIME DEFAULT GETDATE(),
+    userInfo NVARCHAR(255),
+    FOREIGN KEY (lockerId) REFERENCES lockers(lockerId)
+);
+
+-- SEAT TYPES
+CREATE TABLE seatTypes (
+    seatTypeId INT PRIMARY KEY,
+    seatTypeName VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+-- SEATS
+CREATE TABLE seats (
+    seatId INT IDENTITY(1,1) PRIMARY KEY,
+    roomId INT,
+    seatRow CHAR(1),
+    seatColumn INT,
+    seatTypeId INT,
+    isActive BIT,
+    FOREIGN KEY (seatTypeId) REFERENCES seatTypes(seatTypeId),
+    FOREIGN KEY (roomId) REFERENCES screeningRooms(roomId)
+);
+
+-- TICKET SEATS
+CREATE TABLE ticketSeats (
+    ticketId INT NOT NULL,
+    seatId INT NOT NULL,
+    FOREIGN KEY (ticketId) REFERENCES tickets(ticketId),
+    FOREIGN KEY (seatId) REFERENCES seats(seatId)
+);
+
+-- TICKET SERVICES
+CREATE TABLE ticketServices (
+    id INT IDENTITY(1,1) PRIMARY KEY,
+    ticketId INT NOT NULL,
+    serviceId INT NOT NULL,
+    quantity INT NOT NULL DEFAULT 1,
+    servicePrice DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (ticketId) REFERENCES tickets(ticketId),
+    FOREIGN KEY (serviceId) REFERENCES services(serviceId)
+);
+
+-- TICKET PAYMENTS
+CREATE TABLE ticketPayments (
+    paymentId INT IDENTITY(1,1) PRIMARY KEY,
+    ticketId INT NOT NULL,
+    paymentMethod VARCHAR(50) NOT NULL,
+    paidAmount DECIMAL(10,2) NOT NULL,
+    paymentTime DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (ticketId) REFERENCES tickets(ticketId)
+);
+
+-- INVOICES
+CREATE TABLE invoices (
+    invoiceId INT PRIMARY KEY IDENTITY(1,1),
+    invoiceNumber VARCHAR(50) UNIQUE NOT NULL,
+    ticketCode VARCHAR(50),
+    employeeId VARCHAR(20),
+    customerInfo VARCHAR(200),
+    subtotal DECIMAL(10,2),
+    tax DECIMAL(10,2),
+    discount DECIMAL(10,2),
+    totalAmount DECIMAL(10,2),
+    paymentMethod VARCHAR(50),
+    receivedAmount DECIMAL(10,2),
+    changeAmount DECIMAL(10,2),
+    createdAt DATETIME DEFAULT GETDATE()
+);
+
+-- INDEXES
+CREATE INDEX idx_locker_assignments_ticket ON lockerAssignments(ticketCode);
+
+-- ========== INSERT TEST DATA ========== --
+
+-- USERS
+INSERT INTO users (employeeId, username, passwordHash, fullName, email, phone, role)
+VALUES
+    ('EMP001', 'admin', '$2a$10$PfalOMwpyFKOUcDoxFbFlu6qgLzZ/9vhYJ97TnmceKrDaZLTF6qhy', N'Tran Hai Dang', 'admin@example.com', '0901234567', 'Admin'),
+    ('EMP002', 'manager', '$2a$10$PfalOMwpyFKOUcDoxFbFlu6qgLzZ/9vhYJ97TnmceKrDaZLTF6qhy', N'Duong Ngoc Han', 'manager1@example.com', '0912345678', 'Manager'),
+    ('EMP003', 'employee', '$2a$10$PfalOMwpyFKOUcDoxFbFlu6qgLzZ/9vhYJ97TnmceKrDaZLTF6qhy', N'Pham The Duyet', 'employee1@example.com', '0923456789', 'Employee');
+
+-- ROOM TYPES
+INSERT INTO roomTypes (typeName, description, maxRows, maxColumns)
+VALUES
+    (N'Standard', N'Standard Room', 10, 15),
+    (N'Gold', N'High Room', 8, 12);
+
+-- MOVIES
+INSERT INTO movies (title, duration, genre, description, directedBy, language, poster, ageRating, releaseDate)
+VALUES
+    (N'Avengers: Endgame', 181, 'Action, Sci-Fi', N'Final showdown against Thanos.', 'Anthony and Joe Russo', 'English', 'endgame.jpg', 'PG-13', '2019-04-26'),
+    (N'Parasite', 132, 'Drama, Thriller', N'Korean class conflict drama.', 'Bong Joon Ho', 'Korean', 'parasite.jpg', 'R', '2019-05-30');
+
+-- SEAT TYPES
+INSERT INTO seatTypes (seatTypeId, seatTypeName, price)
+VALUES
+    (1, 'Standard', 75000),
+    (2, 'VIP', 100000),
+    (3, 'Sweetbox', 130000),
+    (4, 'Gold', 150000);
